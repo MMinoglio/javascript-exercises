0fbed2dcb4de93180ac6adc01cfd3e9b8cb92f62
MMinoglio <martin.minoglio@outlook.com>
2021-12-25 15:38:33 -0300

    resuelto 03-reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
 const reverseString = function(str) {
 
+let reversed = ""
+
+  for (i=str.length-1 ; i >= 0 ; i--) {
+    reversed += str[i];
+  }
+  return reversed;
 };
 
 // Do not edit below this line
